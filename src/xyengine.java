507a4bc67a5076fb09a145d81ec3771d91eacd67
Dian Yordanov <dido.jordanov7@gmail.com>
2015-03-14 14:30:47 +0000

    commit 1

--- src/xyengine.java
+++ src/xyengine.java
@@ -1,6 +1,12 @@
 /**
  * Created by XelnectMobileUser on 3/14/2015.
  */
-public class xyengine {
-}
+public abstract class xyengine {
 // For the swarm!!!
+//xyengine as it is now can be used for methods but it is not to be used for other types of information
+
+
+    public static void helloWorldExample(){
+System.out.print("hello world");
+    }
+}
